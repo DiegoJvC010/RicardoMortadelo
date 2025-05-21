0a0de81cdece144a0d74e080e78001f59b469ed7
DiegoJvC010 <98868955+DiegoJvC010@users.noreply.github.com>
2025-05-21 00:24:39 -0600

    Update composeApp.mjs

--- docs/composeApp.mjs
+++ docs/composeApp.mjs
@@ -1,11 +1,11 @@
 
 import * as Li9za2lrby5tanM from 'skiko.mjs';
 import * as _ref_QGpzLWpvZGEvY29yZQ_ from '@js-joda/core';
-import { instantiate } from './composeApp.uninstantiated.mjs';
+import { instantiate } from 'composeApp.uninstantiated.mjs';
 import "./custom-formatters.js"
 
 const exports = (await instantiate({
-    './skiko.mjs': Li9za2lrby5tanM,
+    'skiko.mjs': Li9za2lrby5tanM,
     '@js-joda/core': _ref_QGpzLWpvZGEvY29yZQ_
 })).exports;
 
